1795e68e6f8beb2eeddecc2a0c6de95d1af561c8
SamuCoco04 <cocodelfasamuel04@gmail.com>
2024-03-19 18:31:08 +0100

    solucion_error_migrations

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -1,87 +1,95 @@
-import { Model } from 'sequelize'
-import moment from 'moment'
-
-const loadModel = (sequelize, DataTypes) => {
-  class Restaurant extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate (models) {
-      // define association here
-      Restaurant.belongsTo(models.RestaurantCategory, { foreignKey: 'restaurantCategoryId', as: 'restaurantCategory' })
-      Restaurant.belongsTo(models.User, { foreignKey: 'userId', as: 'user' })
-      Restaurant.hasMany(models.Product, { foreignKey: 'restaurantId', as: 'products' })
-      Restaurant.hasMany(models.Order, { foreignKey: 'restaurantId', as: 'orders' })
-    }
-
-    async getAverageServiceTime () {
-      try {
-        const orders = await this.getOrders()
-        const serviceTimes = orders.filter(o => o.deliveredAt).map(o => moment(o.deliveredAt).diff(moment(o.createdAt), 'minutes'))
-        return serviceTimes.reduce((acc, serviceTime) => acc + serviceTime, 0) / serviceTimes.length
-      } catch (err) {
-        return err
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Restaurants', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      shippingCosts: {
+        allowNull: false,
+        defaultValue: 0.0,
+        type: Sequelize.DOUBLE
+      },
+      averageServiceMinutes: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      logo: {
+        type: Sequelize.STRING
+      },
+      heroImage: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: [
+          'online',
+          'offline',
+          'closed',
+          'temporarily closed'
+        ],
+        defaultValue: 'offline'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: {
+            tableName: 'Users'
+          },
+          key: 'id'
+        }
+      },
+      restaurantCategoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
       }
-    }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Restaurants')
   }
-  Restaurant.init({
-    name: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    description: DataTypes.TEXT,
-    address: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    postalCode: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    url: DataTypes.STRING,
-    shippingCosts: {
-      allowNull: false,
-      type: DataTypes.DOUBLE
-    },
-    averageServiceMinutes: DataTypes.DOUBLE,
-    email: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    logo: DataTypes.STRING,
-    heroImage: DataTypes.STRING,
-    status: {
-      type: DataTypes.ENUM,
-      values: [
-        'online',
-        'offline',
-        'closed',
-        'temporarily closed'
-      ]
-    },
-    restaurantCategoryId: {
-      allowNull: false,
-      type: DataTypes.INTEGER
-    },
-    userId: {
-      allowNull: false,
-      type: DataTypes.INTEGER
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-      defaultValue: new Date()
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-      defaultValue: new Date()
-    }
-  }, {
-    sequelize,
-    modelName: 'Restaurant'
-  })
-  return Restaurant
 }
-export default loadModel
-
